b0d4ec24411db055a2dcf5c6fcda57c68f064d74
Prince <princeking950@gmail.com>
2023-09-19 22:52:44 -0700

    dummy data

--- src/app/components/Feed.jsx
+++ src/app/components/Feed.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Stories from './Stories'
+import Posts from './Posts'
 
 export default function Feed() {
   return (
@@ -7,7 +8,7 @@ export default function Feed() {
         <section>
             <Stories />
 
-            {/* posts */}
+            <Posts />
         </section>
 
         <section>
---
+++ src/app/components/Posts.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Post from './Post'
+
+export default function Posts() {
+    const posts = [
+        {
+            id: '1',
+            username:"codewithhammer",
+            userImg: "/boredape.jpg",
+            img: "/ba1.png",
+            caption: 'Nice car'
+        }
+    ]
+  return (
+    
+    <div>
+        {posts.map((post) => (
+            <Post 
+                key={post.id}
+                id={post.id}
+                username={post.username}
+                userImg={post.userImg}
+                img={post.img}
+                caption={post.caption}
+            />
+        ))}
+    </div>
+  )
+}
